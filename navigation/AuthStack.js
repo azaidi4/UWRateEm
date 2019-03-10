acd44e5bbb45ea78eecdbbb90cb1eac1f0dd9e60
Ahmad Zaidi <youremail@who.com>
2019-03-10 16:10:01 -0500

    added reset password functionality

--- navigation/AuthStack.js
+++ navigation/AuthStack.js
@@ -2,5 +2,11 @@ import { createStackNavigator } from 'react-navigation';
 
 import LoginScreen from '../screens/auth/LoginScreen';
 import SignupScreen from '../screens/auth/SignupScreen';
+import ForgotPasswordScreen from '../screens/auth/ForgotPasswordScreen';
 
-export default AuthStack = createStackNavigator({ SignIn: LoginScreen, SignUp: SignupScreen });
+export default AuthStack = createStackNavigator(
+    {
+        SignIn: LoginScreen,
+        SignUp: SignupScreen,
+        ForgotPassword: ForgotPasswordScreen
+    });
